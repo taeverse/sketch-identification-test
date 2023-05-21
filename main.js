7ab857137372e80b120cbcc27c79a6cdef3ed512
taeverse <101496251+taeverse@users.noreply.github.com>
2023-05-21 14:28:04 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function setup() {
+    canvas = createCanvas(280, 250);
+    canvas.center();
+    background("white");
+    canvas.moveReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+
+function preload() {
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function draw() {
+    strokeWeight(13);
+    stroke(0)
+
+}
+
+function clearCanvas() {
+    background("white");
+}
+if (mouseIsPressed) {
+    line(pmouseX, pmouseY, mouseX, mouseY);
+}
+
+function classifyCanvas() {
+    classifier, classify(canvas, gotResults);
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    }
+
+    console.log(results)
+    document.getElementById('label').innerHTML = "Label: " + results[0].label;
+
+    document.getElementById('confidence').innerHTML = "Confidence: " + Math.round(results[0].confidence * 100) + '%';
+    utterThis = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
+}
